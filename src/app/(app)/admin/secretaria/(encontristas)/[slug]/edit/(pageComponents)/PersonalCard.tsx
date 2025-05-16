10fa05d3148c65234f5b496308b7f4f70d4f6390
Pugialli <joaopugialli@gmail.com>
2025-05-16 11:52:03 -0300

    block change name for secre

--- src/app/(app)/admin/secretaria/(encontristas)/[slug]/edit/(pageComponents)/PersonalCard.tsx
+++ src/app/(app)/admin/secretaria/(encontristas)/[slug]/edit/(pageComponents)/PersonalCard.tsx
@@ -1,4 +1,5 @@
 import { CardForm } from '@/components/Form/CardForm'
+import { DisabledInput } from '@/components/Form/DisabledInput'
 import { TextInput } from '@/components/Form/TextInput'
 import { FormField } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
@@ -8,14 +9,17 @@ import { useHookFormMask } from 'use-mask-input'
 export function PersonalCard() {
   const form = useFormContext()
 
-  const { register, control } = form
+  const { register, control, watch } = form
 
   const registerWithMask = useHookFormMask(register)
+
+  const nomeValue = watch('nome')
 
   return (
     <CardForm title="Dados Pessoais" sectionId="personal-section">
       <div className="grid grid-cols-1 gap-4 pt-4 lg:grid-cols-2 lg:gap-8">
-        <FormField
+        <DisabledInput label="Nome" value={nomeValue} />
+        {/* <FormField
           control={control}
           name="nome"
           render={({ field }) => (
@@ -23,13 +27,13 @@ export function PersonalCard() {
               <Input autoFocus {...field} />
             </TextInput>
           )}
-        />
+        /> */}
         <FormField
           control={control}
           name="sobrenome"
           render={({ field }) => (
             <TextInput label={'Sobrenome *'}>
-              <Input {...field} />
+              <Input autoFocus {...field} />
             </TextInput>
           )}
         />
