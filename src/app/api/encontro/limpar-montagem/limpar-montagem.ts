97e63abe8f34a689ce94d65380587189e3c082c3
Pugialli <joaopugialli@gmail.com>
2025-07-18 10:41:00 -0300

    corrigir convidados especiais

--- src/app/api/encontro/limpar-montagem/limpar-montagem.ts
+++ src/app/api/encontro/limpar-montagem/limpar-montagem.ts
@@ -1,5 +1,39 @@
 import { prisma } from '@/lib/prisma'
+import { getCurrentEncontro } from '../atual/[ignorar]/get-current-encontro/get-current-encontro'
 
 export async function limparMontagem() {
+  const currentEncontro = await getCurrentEncontro()
+
+  if (!currentEncontro) return null
+
+  const lastEncontro = await prisma.encontro.findUnique({
+    select: { id: true },
+    where: {
+      numeroEncontro: currentEncontro.numeroEncontro - 1,
+    },
+  })
+
+  if (!lastEncontro) return null
+
+  await prisma.encontreiro.updateMany({
+    data: {
+      statusMontagem: 'ATIVO',
+    },
+    where: {
+      statusMontagem: 'CONVIDADO_ESPECIAL',
+    },
+  })
+
+  await prisma.encontreiro.updateMany({
+    data: {
+      statusMontagem: 'CONVIDADO_ESPECIAL',
+    },
+    where: {
+      idEncontro: {
+        in: [currentEncontro.id, lastEncontro.id],
+      },
+    },
+  })
+
   return await prisma.equipeMontagem.deleteMany()
 }
